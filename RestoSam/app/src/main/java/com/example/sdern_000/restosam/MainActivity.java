2d05b0eae8b7f2d0f2454c51745e3149c0bb09a7
Sdernal <sdernal2@gmail.com>
2016-12-01 01:38:06 +0300

    Simple menu added

--- RestoSam/app/src/main/java/com/example/sdern_000/restosam/MainActivity.java
+++ RestoSam/app/src/main/java/com/example/sdern_000/restosam/MainActivity.java
@@ -1,24 +1,43 @@
 package com.example.sdern_000.restosam;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.view.View.OnClickListener;
 
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    Button menu_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menu_button = (Button) findViewById(R.id.menu_button);
+        menu_button.setOnClickListener(this);
     }
 
 
     public void OnButtonClick(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.menu_button:
+                Intent intent = new Intent(this, MenuActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
--- RestoSam/app/src/main/java/com/example/sdern_000/restosam/SecondActivity.java
+++ RestoSam/app/src/main/java/com/example/sdern_000/restosam/SecondActivity.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,25 @@ public class SecondActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_second);
+
+        editText = (TextView) findViewById(R.id.editText);
+        editText.setText("HUY");
+        button = (Button) findViewById(R.id.button);
+
+        OnClickListener oclBtnOk = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Меняем текст в TextView (tvOut)
+                editText.setText("Нажата кнопка ОК");
+            }
+        };
+
+        // присвоим обработчик кнопке OK (btnOk)
+        button.setOnClickListener(oclBtnOk);
+    }
+
+    public void OnClick(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
